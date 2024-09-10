8b3de6f6205f715b5482140f461209904a715d00
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-09-10 13:55:47 -0300

    corrigindo o multiplicação de inteiros

--- codigos/multiplicacao_inteiros.asm
+++ codigos/multiplicacao_inteiros.asm
@@ -1,11 +1,9 @@
 .text
 	li $t0, 12
 	li $t1, 10
-<<<<<<< HEAD
+
 	# $s0 terá o resultado da multiplicação
-=======
-	
->>>>>>> 670e237bc5849e740d4b4c4ad047b23e6cb55066
+
 	mul $s0,$t0,$t1
 	
 	li $v0,1
@@ -13,11 +11,4 @@
 	move $a0,$s0
 	
 	syscall
-	
-	
 
-<<<<<<< HEAD
-	
-=======
-	
->>>>>>> 670e237bc5849e740d4b4c4ad047b23e6cb55066
